b98bdffd785d061fa0d61457fb16e439ddb16e0f
unknown <pedroornelaspego@gmail.com>
2023-04-25 16:50:11 -0300

    update

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -4,35 +4,35 @@ import "./ImageGallery.scss";
 const imagens = [
   {
     nome: "galeria-1",
-    caminho: "../src/assets/galeria-1.png",
+    caminho: "../../src/assets/galeria-1.png",
   },
   {
     nome: "galeria-2",
-    caminho: "../src/assets/galeria-2.png",
+    caminho: "../../src/assets/galeria-2.png",
   },
   {
     nome: "galeria-3",
-    caminho: "../src/assets/galeria-3.png",
+    caminho: "../../src/assets/galeria-3.png",
   },
   {
     nome: "galeria-4",
-    caminho: "../src/assets/galeria-4.png",
+    caminho: "../../src/assets/galeria-4.png",
   },
   {
     nome: "galeria-5",
-    caminho: "../src/assets/galeria-5.png",
+    caminho: "../../src/assets/galeria-5.png",
   },
   {
     nome: "galeria-6",
-    caminho: "../src/assets/galeria-6.png",
+    caminho: "../../src/assets/galeria-6.png",
   },
   {
     nome: "galeria-7",
-    caminho: "../src/assets/galeria-7.png",
+    caminho: "../../src/assets/galeria-7.png",
   },
   {
     nome: "galeria-8",
-    caminho: "../src/assets/galeria-8.png",
+    caminho: "../../src/assets/galeria-8.png",
   },
 ];
 
